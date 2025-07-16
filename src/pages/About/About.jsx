d97a395337451baddabb2c98d494e54d7416a23a
Tyler Welker <twelker08@gmail.com>
2025-07-16 11:47:51 -0400

    new portfolio

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <section id="about" className="py-16 md:py-32  text-white bg-[#04081A]">
         <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
           <h2 className="relative z-10 max-w-xl text-4xl font-medium lg:text-5xl text-white">
-            Developer, Designer, Creator, Innovator
+            Developer, Creator
           </h2>
           <div className="grid gap-6 sm:grid-cols-2 md:gap-12 lg:gap-24">
             <div className="relative mb-6 sm:mb-0">
@@ -52,7 +52,7 @@ export default function About() {
 
                   <div className="mt-6 space-y-3">
                     <cite className="block font-medium text-white">
-                      Nazmul Hossain, Creator of
+                      Tyler Welker
                     </cite>
                     <div className="flex items-center gap-2">
                       <img
