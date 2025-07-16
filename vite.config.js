3e517352d6860b734423950881cc6e4fc6902747
Tyler Welker <twelker08@gmail.com>
2025-07-16 09:00:58 -0400

    new portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio-v.05/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
